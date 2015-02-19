5051da0b0cadab691b405221779eaff8a7b7a4e4
Josh Lubaway <josh.lubaway@gmail.com>
2015-02-18 19:32:18 -0800

    Initial Commit

---
+++ twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitter
+//
+//  Created by Josh Lubaway on 2/18/15.
+//  Copyright (c) 2015 Frustrated Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
